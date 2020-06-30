873b3aaf79942a8efadbe88d0a2572bd5ff3f446
mirsamie <mirsamie@PROGRAMMER11>
2020-06-30 14:19:32 +0430

    .

--- index.js
+++ index.js
@@ -14,14 +14,11 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 // bot.on('sticker', (ctx) => ctx.reply('👍'))
 // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.on('message', (ctx , extra) => {
-    // console.log(ctx, extra)
-    // ctx.reply('a', Extra.inReplyTo(ctx.update.message.message_id))
-    // bot.telegram.sendMessage(ctx.chat.id, 'a', Extra.inReplyTo(ctx.update.message.message_id))
-    // Logic.replyTo(bot, ctx.chat.id, 'a', ctx.update.message.message_id)
-    
-    let parseMessage = new ParseMessage(client, ctx.update.message.text, ctx.update.message.from.id, ctx.update.message.message_id, ctx.chat.id)
-    if(parseMessage.status)
-        Logic.findPrices(client, bot).then().catch(err => console.log('PRICES Error:', err))  
-
+    if(ctx.update.message.text)
+    {
+        let parseMessage = new ParseMessage(client, ctx.update.message.text, ctx.update.message.from.id, ctx.update.message.message_id, ctx.chat.id)
+        if(parseMessage.status)
+            Logic.findPrices(client, bot).then().catch(err => console.log('PRICES Error:', err))  
+    }
 })
 bot.launch()
